1c1f8387dded919c5bd8b696aeba5828058434b2
Kavian Haghighi Javid <DEINE_EMAIL_ADRESSE>
2026-06-10 17:01:57 +0200

    Back 2 Black

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -23,7 +23,7 @@ var GEOLOCATION_API = {
                 "heading": null,
                 "speed": null
             },
-            "timestamp tmm": 1775140116396
+            "timestamp": 1775140116396
         });
     }
 };
